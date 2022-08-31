1d02414c3542530ec9fd89bb2638b13ac6e9bf2d
Steven121x <Steven121x@gmail.com>
2022-08-31 09:21:31 -0400

    Hola

---
+++ ArrayReturn/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayReturn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Elementos = LeerDatos();
+            Console.Clear();
+            Console.WriteLine("Imprimiendo datos desde el main\n\n");
+            foreach(int i in Elementos)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static int[] LeerDatos()
+        {
+            Console.WriteLine("Cuantos elementos quieres que tenga el array");
+            string respuesta = Console.ReadLine();
+            int ElementosRespuesta = int.Parse(respuesta);
+
+            int[] datos = new int[ElementosRespuesta];
+
+            for (int i = 0; i < ElementosRespuesta; i++)
+            {
+                Console.WriteLine($"Que datos quieres que se almacene en la pos {i}");
+                respuesta = Console.ReadLine();
+                int RespuestaDato = int.Parse(respuesta);
+                datos[i] = RespuestaDato;
+
+            }
+            return datos;
+        }
+       
+    }
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+🍇Created in Visual Studio 2022🍇
+
+🌎Array by parameter🌎
+
+💥Add Elements💥
+
+🥇Add Values of the Array🥇
+
+🦝Print Values of the Array in Method Main🦝
+
+| -------------------------------------------------------------------------------------- |
+
+🟠ES🟠
+
+🍇Creado en Visual Studio 2022🍇
+
+🌎Array por parametro🌎
+
+💥Asigna elementos💥
+
+🥇Asigna valores de las pos🥇
+
+🦝Imprime los valores del array🦝
+
+✅Dev By ! - Steven#8888✅
+
+🔔Created in 31/8/2022🔔
